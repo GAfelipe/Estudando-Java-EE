a5f754a797e950e036b6a6a9f6ddd8fb7dd68992
GAfelipe <felipe.gabriel.mariz@hotmail.com>
2020-03-03 18:13:14 -0300

    implementação até a página 42

---
+++ src/main/java/com/algaworks/financeiro/model/Pessoa.java
@@ -0,0 +1,27 @@
+package com.algaworks.financeiro.model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
+
+
+
